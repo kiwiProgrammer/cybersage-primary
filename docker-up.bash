d423ee24917f4ab403c5794cb1a0123d68e53e9a
Kit Weng Cheah <kitweng.cheah@stellaautomotive.com>
2025-10-11 16:53:42 +0800

    housekeeping

--- docker-up.bash
+++ docker-up.bash
@@ -11,7 +11,7 @@ echo ""
 cd "${SCRIPT_DIR}"
 
 docker-compose --project-directory ./ \
-  -f AutonomousCouncil/docker-compose.yml \
-  -f security-advisor/docker-compose.yml \
+  -f agent_c/docker-compose.yml \
+  -f agent_d/docker-compose.yml \
   -f docker-compose.yml \
   up --build
